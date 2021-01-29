e770aa91e3b126638c1ed4d25247c3bcfaf9778d
JonathanNash21 <c21jonathan.nash@afacademy.af.edu>
2021-01-29 12:10:21 -0700

    ai train

--- main.py
+++ main.py
@@ -55,3 +55,37 @@ for data_batch, labels_batch in trainGenerator:
 	print('data batch shape:', data_batch.shape)
 	print('labels batch shape:', labels_batch.shape)
 	break
+	
+history = model.fit_generator(
+							trainGenerator,
+							steps_per_epoch=50,
+							epochs=30,
+							validation_data=valGenerator,
+							validation_steps=25)
+							
+model.save('smol_car.h5')
+
+import matplotlib.pyplot as plt
+
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(1, len(acc) + 1)
+
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.legend()
+
+plt.figure()
+
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+
+plt.show()
+
+
