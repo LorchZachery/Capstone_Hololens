ab489071090f41bc82c00dda1863527f1e38fb34
JonathanNash21 <c21jonathan.nash@afacademy.af.edu>
2021-02-04 10:32:09 -0700

    push again

--- instantiate.py
+++ instantiate.py
@@ -1,31 +1,31 @@
 #def smalldir:
 import os, shutil
-original_train_dir = 'D:\CapstoneAI Images\cars_train'
-original_test_dir = 'D:\CapstoneAI Images\cars_test'
+original_train_dir = 'F:\CapstoneAI Images\cars_train'
+original_test_dir = 'F:\CapstoneAI Images\cars_test'
 
-base_dir = 'D:\CapstoneAI small'
-#os.mkdir(base_dir)
+base_dir = 'F:\CapstoneAI big'
+os.mkdir(base_dir)
 
 train_dir = os.path.join(base_dir, 'train')
-#os.mkdir(train_dir)
+os.mkdir(train_dir)
 val_dir = os.path.join(base_dir, 'validation')
-#os.mkdir(val_dir)
+os.mkdir(val_dir)
 test_dir = os.path.join(base_dir, 'test')
-#os.mkdir(test_dir)
+os.mkdir(test_dir)
 
-fnames = ['{}.jpg'.format(i) for i in range(1,1001)]
+fnames = ['{}.jpg'.format(i) for i in range(1,6108)]
 for fname in fnames:
     src = os.path.join(original_train_dir, fname)
     dst = os.path.join(train_dir, fname)
     shutil.copyfile(src, dst)
 
-fnames = ['{}.jpg'.format(i) for i in range(1001, 1501)]
+fnames = ['{}.jpg'.format(i) for i in range(6109, 8144)]
 for fname in fnames:
     src = os.path.join(original_train_dir, fname)
     dst = os.path.join(val_dir, fname)
     shutil.copyfile(src, dst)
 
-fnames = ['{}.jpg'.format(i) for i in range(1,501)]
+fnames = ['{}.jpg'.format(i) for i in range(1,8041)]
 for fname in fnames:
     src = os.path.join(original_test_dir, fname)
     dst = os.path.join(test_dir, fname)
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from keras.preprocessing.image import ImageDataGenerator
 original_train_dir = 'F:\CapstoneAI Images\cars_train'
 original_test_dir = 'F:\CapstoneAI Images\cars_test'
 
-base_dir = 'F:\CapstoneAI small'
+base_dir = 'F:\CapstoneAI big'
 #os.mkdir(base_dir)
 
 train_dir = os.path.join(base_dir, 'train')
@@ -26,6 +26,7 @@ model.add(layers.MaxPooling2D((2,2)))
 model.add(layers.Conv2D(128, (3, 3), activation='relu'))
 model.add(layers.MaxPooling2D((2,2)))
 model.add(layers.Flatten())
+model.add(layers.Dropout(0.5))
 model.add(layers.Dense(512, activation='relu'))
 model.add(layers.Dense(1, activation='sigmoid'))
 
@@ -36,19 +37,25 @@ from keras import optimizers
 
 model.compile(loss='binary_crossentropy', optimizer=optimizers.RMSprop(lr=1e-4),metrics=['acc']) 
 
-trainDatagen = ImageDataGenerator(rescale=1./255)
+trainDatagen = ImageDataGenerator(rescale=1./255,
+						rotation_range=40,
+						width_shift_range=0.2,
+						height_shift_range=0.2,
+						shear_range=0.2,
+						zoom_range=0.2,
+						horizontal_flip=True)
 testDatagen = ImageDataGenerator(rescale=1./255)
 
 trainGenerator = trainDatagen.flow_from_directory(
 												train_dir,
 												target_size=(150, 150),
-												batch_size=20,
+												batch_size=100,
 												class_mode='binary')
 
 valGenerator = testDatagen.flow_from_directory(
 												val_dir,
 												target_size=(150,150),
-												batch_size=20,
+												batch_size=100,
 												class_mode='binary')
 
 for data_batch, labels_batch in trainGenerator:
@@ -58,12 +65,12 @@ for data_batch, labels_batch in trainGenerator:
 	
 history = model.fit_generator(
 							trainGenerator,
-							steps_per_epoch=50,
-							epochs=30,
+							steps_per_epoch=61,
+							epochs=100,
 							validation_data=valGenerator,
-							validation_steps=25)
+							validation_steps=21)
 							
-model.save('smol_car.h5')
+model.save('big_car_better.h5')
 
 import matplotlib.pyplot as plt
 
@@ -87,5 +94,45 @@ plt.title('Training and validation loss')
 plt.legend()
 
 plt.show()
+"""
+
+################################## Data augmentation
+
+datagen = ImageDataGenerator(
+						rotation_range=40,
+						width_shift_range=0.2,
+						height_shift_range=0.2,
+						shear_range=0.2,
+						zoom_range=0.2,
+						horizontal_flip=True,
+						fill_mode='nearest')
+from keras.preprocessing import image
+
+for fname in os.listdir(train_dir):
+	sub_dir = os.path.join(train_dir, fname) 
+
+#print(sub_dir)
+
+fnames = [os.path.join(sub_dir, fname) for fname in os.listdir(sub_dir)]
+
+#print(fnames)
+img_path = fnames[3]
+
+img = image.load_img(img_path, target_size=(150, 150))
+
+x = image.img_to_array(img)
+
+x = x.reshape((1,) + x.shape)
+i = 0
+
+for batch in datagen.flow(x, batch_size=1):
+	plt.figure(i)
+	imgplot = plt.imshow(image.array_to_img(batch[0]))
+	i += 1
+	if i % 4 == 0:
+		print('Hit break, i = ', i)
+		break
+plt.show()
 
 
+"""
